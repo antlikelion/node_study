bccfb3d73dfaaed510f14d056a950945520f3efc
antlikelion <ignorant_woo@naver.com>
2019-09-09 16:08:57 +0900

    yargs모듈로 사용자 입력과 command 바인딩?

--- notes/app.js
+++ notes/app.js
@@ -1,12 +1,46 @@
 const notes = require('./notes')
 const chalk = require('chalk')
+const yargs = require('yargs')
 
-const command = process.argv[2]
 
-console.log(process.argv)
+// customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
+
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function () {
+        console.log('Removing the note!')
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes!')
+    }
+})
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note!')
+    }
+})
+
+
+console.log(yargs.argv)
