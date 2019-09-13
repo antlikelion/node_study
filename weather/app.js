11b1a9fa79645391fafd07b5c1bd04368efd3033
antlikelion <ignorant_woo@naver.com>
2019-09-13 12:46:32 +0900

    HTTP 요청 커스텀

--- weather/app.js
+++ weather/app.js
@@ -1,8 +1,10 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/eeb51ccfe4648db3a9a4340f355f3c70/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/eeb51ccfe4648db3a9a4340f355f3c70/37.8267,-122.4233?lang=ko'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    let summary = response.body.daily.data[0].summary
+    let degree = response.body.currently.temperature
+    let chanceOfRain = response.body.currently.precipProbability
+    console.log(`${summary} It is currently ${degree} degrees out. There is a ${chanceOfRain * 100}% chance of rain`)
 })
