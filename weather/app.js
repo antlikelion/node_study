412c458792179659dffe927939751a096e38a87d
antlikelion <ignorant_woo@naver.com>
2019-09-13 19:58:40 +0900

    destructuring을 통한 refactoring

--- weather/app.js
+++ weather/app.js
@@ -5,15 +5,15 @@ let place = process.argv[2]
 if (!place) {
     return console.log('주소를 입력해주세요')
 } else {
-    geocode(place, (error, data) => {
+    geocode(place, (error, { latitude, longtitude, location }) => { // data destructuring
         if (error) {
             return console.log('Error', error)
         } // if else 안쓰기 위해 return 구문 썼음
-        forecast(data.latitude, data.longtitude, (error, forecastData) => {
+        forecast(latitude, longtitude, (error, forecastData) => {
             if (error) {
                 return console.log('Error', error)
             } // if else 안쓰기 위해 return 구문 썼음
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
         })
     })
