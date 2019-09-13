25b80e2c164de35422141be6fe31e69785a1d02b
antlikelion <ignorant_woo@naver.com>
2019-09-13 12:00:06 +0900

    request모듈 소개

--- weather/app.js
+++ weather/app.js
@@ -1,12 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/eeb51ccfe4648db3a9a4340f355f3c70/37.8267,-122.4233'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-
-console.log('Stopping')
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+})
