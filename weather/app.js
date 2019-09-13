863871c41c169e8d2d2659ec0a98abafaaddf40e
antlikelion <ignorant_woo@naver.com>
2019-09-13 18:33:53 +0900

    forecast함수 모듈화

--- weather/app.js
+++ weather/app.js
@@ -1,39 +1,15 @@
-const request = require('request')
-
 const geocode = require('./utils/geocode')
-
-// const url = 'https://api.darksky.net/forecast/eeb51ccfe4648db3a9a4340f355f3c70/37.8267,-122.4233?lang=ko'
-
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         let summary = response.body.daily.data[0].summary
-//         let degree = response.body.currently.temperature
-//         let chanceOfRain = response.body.currently.precipProbability
-//         console.log(`${summary} It is currently ${degree} degrees out. There is a ${chanceOfRain * 100}% chance of rain`)
-//     }
-// })
-
-// const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW50bGlrZWxpb24iLCJhIjoiY2swaGtud3Z4MDNsMjNocGk3d2d3bGVzeiJ9.oOpboYAopMLirRQgMXCmOg&language=ko&limit=1'
-
-// request({ url: geoUrl, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to geocoding service!')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find location')
-//     } else {
-//         let latitude = response.body.features[0].center[1]
-//         let longtitude = response.body.features[0].center[0]
-//         console.log(`위도 : ${latitude}, 경도 : ${longtitude}`)
-//     }
-// })
-
-
+const forecast = require('./utils/forecast')
 
 geocode('London', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
+
+module.exports = geocode
+
+
+forecast(-75.7088, 44.1545, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
