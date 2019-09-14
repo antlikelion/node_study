5e28bcd2b58ee26421d7a8394349b911d525efd0
antlikelion <ignorant_woo@naver.com>
2019-09-14 20:44:00 +0900

    브라우저에서 입력 값을 받아 클라이언트 사이드 자바스크립트로 fetch요청 보내기

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,12 +6,30 @@ console.log('Client side javascript file is loaded!')
 //     })
 // })
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            return console.log(`오류 : ${data.error}`)
-        }
-        console.log(`지역 : ${data.location}`)
-        console.log(`예보 : ${data.forecast}`)
+// fetch('http://localhost:3000/weather?address=boston').then((response) => {
+//     response.json().then((data) => {
+//         if (data.error) {
+//             return console.log(`오류 : ${data.error}`)
+//         }
+//         console.log(`지역 : ${data.location}`)
+//         console.log(`예보 : ${data.forecast}`)
+//     })
+// })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return console.log(`오류 : ${data.error}`)
+            }
+            console.log(`지역 : ${data.location}`)
+            console.log(`예보 : ${data.forecast}`)
+        })
     })
 })
