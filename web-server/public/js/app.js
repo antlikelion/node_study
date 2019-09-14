f688e563ef4020d045a7944b5086d0c026eede36
antlikelion <ignorant_woo@naver.com>
2019-09-14 20:05:32 +0900

    클라이언트 사이드 javascript로 fetch요청 보내기

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
 console.log('Client side javascript file is loaded!')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+fetch('http://localhost:3000/weather?address=!').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.log(`오류 : ${data.error}`)
+        }
+        console.log(`지역 : ${data.location}`)
+        console.log(`예보 : ${data.forecast}`)
+    })
+})
--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiYW50bGlrZWxpb24iLCJhIjoiY2swaGtud3Z4MDNsMjNocGk3d2d3bGVzeiJ9.oOpboYAopMLirRQgMXCmOg&language=ko&limit=1`
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
