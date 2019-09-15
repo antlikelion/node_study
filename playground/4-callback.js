137b136e004f1aba8762983cd239fe74b314c900
antlikelion <ignorant_woo@naver.com>
2019-09-15 18:07:00 +0900

    프로미스 소개

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -1,43 +1,13 @@
-// setTimeout(() => {
-//     console.log('Two seconds are up')
-// }, 2000)
-
-// const names = ['이인우', '인우', '우']
-// constshortNames = names.filter((name) => {
-//     return name.length <= 2
-// })
-
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitude: 0,
-//             longtitude: 0
-//         }
-//         callback(data)
-//     }, 2000)
-// }
-
-// geocode('Philadelphia', (data) => {
-//     console.log(data)
-// })
-
-
-
-//
-// Goal: Mess around with the callback pattern
-//
-// 1. Define an add function that accepts the correct arguments
-// 2. Use setTimeout to simulate a 2 second delay
-// 3. After 2 seconds are up, call the callback function with the sum
-// 4. Test your work!
-
-const add = (num1, num2, callback) => {
-    let sum = num1 + num2
+const doWorkCallback = (callback) => {
     setTimeout(() => {
-        callback(sum)
+        // callback('This is my error', undefined)
+        callback(undefined, [1, 4, 7])
     }, 2000)
 }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+    console.log(result)
 })
