0b953b7cf29f907dc4d61bff3a6a306de4833604
antlikelion <ignorant_woo@naver.com>
2019-09-15 17:33:56 +0900

    몽고디비 Read

--- task/mongodb.js
+++ task/mongodb.js
@@ -9,58 +9,36 @@ const connectionURL = 'mongodb://127.0.0.1:27017'
 // 127.0.0.1 대신 localhost를 쓰면 속도가 느려지는 등 여러 문제가 있다고 함.
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id.id.length)
-console.log(id.toHexString().length)
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database')
     }
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: '우인이',
-        age: 28
-    }, (error, result) => {
+    // db.collection('users').findOne({ _id: new ObjectID("5d7de8dd9514bf43f01eec7a") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch')
+    //     }
+
+    //     console.log(user)
+    // })
+
+    // db.collection('users').find({ age: 28 }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+
+    // db.collection('users').find({ age: 28 }).count((error, count) => {
+    //     console.log(count)
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID("5d7de4ba114d6c63f825f08e") }, (error, task) => {
         if (error) {
-            return console.log('Unable to insert user')
+            return console.log(error)
         }
-        console.log(result.ops)
+        console.log(task)
     })
 
-    // db.collection('users').insertMany([{
-    //     name: '홍길동',
-    //     age: 50
-    // }, {
-    //     name: '임꺽정',
-    //     age: 60
-    // }], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         describe: '전공중국어2 1과 회화1 암송',
-    //         boolean: false
-    //     },
-    //     {
-    //         describe: '60db회식',
-    //         boolean: false
-    //     },
-    //     {
-    //         describe: '파이빌 커피타임',
-    //         boolean: false
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('오류가 났군요')
-    //     }
-
-    //     console.log(result.ops)
-    // })
+    db.collection('tasks').find({ boolean: false }).toArray((error, tasks) => {
+        console.log(tasks)
+    })
 })
