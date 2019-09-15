fbade6f80982a675cfde0efcaec2751b9e687dad
antlikelion <ignorant_woo@naver.com>
2019-09-15 23:52:01 +0900

    몽구스로 모델 커스텀하기

--- task/src/db/mongoose.js
+++ task/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,23 +8,42 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: "이인우",
-//     age: 28
-// })
+const me = new User({
+    name: "    인우이    ",
+    email: " MYEMAIL@NAVER.COM   "
+})
 
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+})
 
 const Task = mongoose.model('Task', {
     description: {
@@ -34,13 +54,13 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const chinese = new Task({
-    description: "중국어회화2 예습",
-    completed: true
-})
+// const chinese = new Task({
+//     description: "중국어회화2 예습",
+//     completed: true
+// })
 
-chinese.save().then((result) => {
-    console.log(result)
-}).catch((error) => {
-    console.log(error)
-})
+// chinese.save().then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
