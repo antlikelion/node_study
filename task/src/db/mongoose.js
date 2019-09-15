0a7713ed343a21f4a6cbf08b0ccd6303d2fa2c33
antlikelion <ignorant_woo@naver.com>
2019-09-15 23:01:28 +0900

    몽구스로 모델 만들고 인스턴스 저장하기

--- task/src/db/mongoose.js
+++ task/src/db/mongoose.js
@@ -14,13 +14,33 @@ const User = mongoose.model('User', {
     }
 })
 
-const me = new User({
-    name: "이인우",
-    age: 28
+// const me = new User({
+//     name: "이인우",
+//     age: 28
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
 })
 
-me.save().then(() => {
-    console.log(me)
+const chinese = new Task({
+    description: "중국어회화2 예습",
+    completed: true
+})
+
+chinese.save().then((result) => {
+    console.log(result)
 }).catch((error) => {
-    console.log('Error!', error)
+    console.log(error)
 })
