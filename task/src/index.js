38e2b91e5f1cc0c90f2aa0b6103be101048c34d9
antlikelion <ignorant_woo@naver.com>
2019-09-16 13:27:29 +0900

    async/await으로 promise chaining 리팩토링하기

--- task/src/index.js
+++ task/src/index.js
@@ -9,65 +9,104 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then((result) => {
-        res.send(result)
-    }).catch((e) => {
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users)
-    }).catch((e) => {
-        res.status(500).send()
-    })
+app.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({})
+        res.status(200).send(users)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
     // 몽고디비 모듈과는 달리 몽구스 디비는 ObjectID로 변환해줄 필요가 없음
-    User.findById(_id).then((user) => {
-        if (!user) { // 해당 id에 맞는 user가 없어도 reject되지 않고 null값을 반환하기 때문에 if문 활용
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
             return res.status(404).send()
         }
-        res.send(user)
-    }).catch((e) => {
-        res.status(500).send(e)
-    })
+        res.status(200).send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+    // User.findById(_id).then((user) => {
+    //     if (!user) { // 해당 id에 맞는 user가 없어도 reject되지 않고 null값을 반환하기 때문에 if문 활용
+    //         return res.status(404).send()
+    //     }
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(500).send(e)
+    // })
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then((result) => {
-        res.send(result)
-    }).catch((e) => {
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
+    // task.save().then((result) => {
+    //     res.send(result)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((e) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).send(tasks)
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
-    Task.findById(_id).then((task) => {
-        if (!task) { // 해당 description에 맞는 user가 없어도 reject되지 않고 null값을 반환하기 때문에 if문 활용
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
             return res.status(404).send()
         }
-        res.send(task)
-    }).catch((e) => {
+        res.status(200).send(task)
+    } catch (e) {
         res.status(500).send(e)
-    })
+    }
+    // Task.findById(_id).then((task) => {
+    //     if (!task) { // 해당 description에 맞는 user가 없어도 reject되지 않고 null값을 반환하기 때문에 if문 활용
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(500).send(e)
+    // })
 })
 
 app.listen(port, () => {
